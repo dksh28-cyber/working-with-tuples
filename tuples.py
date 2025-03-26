2658d4faa1a529dcd559976e86f4d2d745ee5cdf
dksh28-cyber <dkshsingh2019@gmail.com>
2025-03-26 16:26:52 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,30 @@
+players=(1,2,3,4,5,6)
+def func1():
+    print(len(players))
+
+alphabets=(1,2,3,4,5,1,4,1,2,1,2,1)
+def func2():
+    print(alphabets.count(1))
+
+digits=(1,2,3,4,5,6)
+def func3():
+    print(digits.index(2))
+
+tuples=(10,30,40,50,10,20,40)
+def func4():
+    print(sorted(tuples))
+
+minimum=(9,7,3,5,1)
+def func5():
+    print(min(minimum))
+
+maximum=(22,44,33,66,99)
+def func6():
+    print(max(maximum))
+    
+func1()
+func2()
+func3()
+func4()
+func5()
+func6()
